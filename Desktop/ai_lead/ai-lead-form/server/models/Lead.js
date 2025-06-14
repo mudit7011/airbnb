ee513273a314208d6341a72fe0d95db6a04d8f24
Mudit Kumar <muditchaudhary90@gmail.com>
2025-06-14 13:39:08 +0530

    Removed openai added gemini for better responses

---
+++ Desktop/ai_lead/ai-lead-form/server/models/Lead.js
@@ -0,0 +1,27 @@
+// server/models/Lead.js
+import mongoose from "mongoose";
+
+const LeadSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  score: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("Lead", LeadSchema);
